48be2083ad89c1805affaae0421304ae8dc14275
Jesus David <jesusdavidlozanomarin@gmail.com>
2025-01-25 19:31:27 -0400

    Distribucion de componentes y estilos base.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import FileBar from "./components/FIleBar";
+import FilePanel from "./components/FilesPanel";
+import CodeArea from "./components/CodeArea";
+
 const App: React.FC = () => {
     return(
         <div className="App">
-
+            <FileBar/>
+            <div className="workArea">
+                <FilePanel/>
+                <CodeArea/>
+            </div>
         </div>
     )
 }
